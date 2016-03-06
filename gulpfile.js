77342366c2311dbb074b6f50ef03c6b4e2e0e276
Amanda Milke <amandalmilke@gmail.com>
2016-03-06 14:01:59 -0500

    added updatges

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
+var stylus = require('gulp-stylus');
 
 gulp.task('default', function(){
-
+	return gulp.src('./styles/styles.styl')
+		.pipe(stylus())
+		.pipe(gulp.dest('./styles/'));
 });
